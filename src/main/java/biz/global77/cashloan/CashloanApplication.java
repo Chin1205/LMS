3f63fb5e9dbb3f923d4d5f5b52805218b988dc23
ronanborja <ronanborja1205@gmail.com>
2022-12-27 21:41:22 +0800

    Initial commit

---
+++ src/main/java/biz/global77/cashloan/CashloanApplication.java
@@ -0,0 +1,13 @@
+package biz.global77.cashloan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashloanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashloanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/biz/global77/cashloan/CashloanApplicationTests.java
@@ -0,0 +1,13 @@
+package biz.global77.cashloan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashloanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
